f53fb8449ad81ec784c8c095052f27ffbb916c49
andre <andre@10.33.1.144>
2022-10-27 16:42:35 -0300

    Semana 5 - Listagem dos dados

--- Estacionamento/src/model/dao/VagaDAO.java
+++ Estacionamento/src/model/dao/VagaDAO.java
@@ -7,6 +7,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class VagaDAO {
     public void create(Vaga v){
@@ -27,4 +30,30 @@ public class VagaDAO {
         } 
 
 }
+public List<Vaga> read(){
+ Connection con = ConnectionFactory.getConnection();
+    PreparedStatement stmt = null;
+    ResultSet rs = null; 
+    List<Vaga> vagas = new ArrayList<>();
+    try{
+     stmt = con.prepareStatement ("SELECT * FROM vaga;");
+     rs = stmt.executeQuery();
+     while (rs.next()){
+         Vaga v = new Vaga ();
+         v.setIdVaga (rs.getInt ("idVaga"));
+         v.setNumero (rs.getInt ("numero"));
+         v.setRua (rs.getString ("rua"));
+         v.setObliqua (rs.getBoolean ("obliqua"));
+         vagas.add (v);
+     }
+}catch (SQLException e){
+    throw new RuntimeException ("Erro ao buscar os dados: ", e);   
+}finally{
+    ConnectionFactory.closeConnection (con, stmt, rs);
+    }
+    return vagas;
+
+}
+
+
 }
